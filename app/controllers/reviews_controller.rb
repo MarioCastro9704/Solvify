13522e15b235ca2f26f6f4aefbae7dafddf4d181
Eleazar Estrada <elestradariffo@gmail.com>
2024-06-20 18:14:28 -0500

    Controller listos

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,47 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.all
   end
 
   def show
+    @review = Review.find(params[:id])
   end
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to @review, notice: 'Review was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
 
   def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to @review, notice: 'Review was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_url, notice: 'Review was successfully destroyed.'
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comments, :ratings, :psychologist_id)
   end
 end
