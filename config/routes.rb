f7f12c14c6d3563c6321fa7599702fbfcc9e418e
Juan Cruz Dovzak <juandovzak@gmail.com>
2024-06-27 19:11:58 -0300

    Tratando de solucionar algo

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  root to: 'pages#home'
+  get 'pages/home', to: 'pages#home', as: 'pages_home'
+  root to: 'users#index'
 
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
     resources :bookings, only: [:index]
   end
 
   resources :psychologists do
+    member do
+      patch 'update_availabilities'
+    end
     resources :availabilities, only: [:index, :show, :new, :create, :edit, :update, :destroy]
     resources :reviews, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-    resources :clientes, only: [:index]  # Aquí cambiamos de patients a clientes
+    resources :clientes, only: [:index]
   end
 
   resources :bookings, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-
+  resources :services, only: [:new, :create, :edit, :update] do
+    member do
+      patch :publish
+    end
+  end
   get "up" => "rails/health#show", as: :rails_health_check
 end
