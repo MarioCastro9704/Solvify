5dd035e0e713fe6c30f68577456d941115789a38
Juan Cruz Dovzak <juandovzak@gmail.com>
2024-06-27 20:29:27 -0300

    Turbo implementado, cambios a stimulus

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,9 @@
-import { application } from "controllers/application"
-import { definitionsFromContext } from "@hotwired/stimulus-loading"
-import "@popperjs/core"
-import "bootstrap"
-const context = require.context("controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus = application
+window.Stimulus   = application
 
 export { application }
