647b7c2d23ac3415a2ab61406fd500f0936700b8
Juan Cruz Dovzak <juandovzak@gmail.com>
2024-06-27 19:01:52 -0300

    Routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'users#index'
+  root to: 'pages#home'
 
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
     resources :bookings, only: [:index]
