f55530ada1326c398010f175a053a6983beff2a9
Eleazar Estrada <elestradariffo@gmail.com>
2024-06-28 21:46:50 -0500

    Ajustes en los buttons para activar disponibilidad

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,14 +1,27 @@
-// app/javascript/controllers/menu_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menuIcon", "closeIcon"]
+  static targets = ["button"];
 
-  connect() {
-  }
+  toggle(event) {
+    event.preventDefault();
 
-  toggle() {
-    this.menuIconTarget.classList.toggle('d-none')
-    this.closeIconTarget.classList.toggle('d-none')
+    const button = event.currentTarget;
+
+    // Cambiar clases y texto del botón al hacer clic
+    button.classList.toggle('btn-primary');
+    button.classList.toggle('btn-outline-primary');
+
+    if (button.classList.contains('btn-primary')) {
+      button.textContent = 'Disponible';
+    } else {
+      button.textContent = 'No disponible';
+    }
+
+    // Aquí puedes añadir la lógica para enviar datos al servidor si es necesario
+    const dayIndex = this.data.get("toggleDay");
+    const hour = this.data.get("toggleHour");
+    const isChecked = button.classList.contains('btn-primary');
+    console.log(`Day: ${dayIndex}, Hour: ${hour}, Checked: ${isChecked}`);
   }
 }
