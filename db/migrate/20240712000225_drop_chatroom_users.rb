ae2fc92b4d690440a3e26b9f32070a36b26ff51d
MarioCastro <mariocastro9704@gmail.com>
2024-07-11 21:03:57 -0400

    integración con chat

--- db/migrate/20240712000225_drop_chatroom_users.rb
+++
@@ -1,5 +0,0 @@
-class DropChatroomUsers < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :chatrooms_users
-  end
-end
--- db/migrate/20240712000354_drop_chatrooms.rb
+++
@@ -1,5 +0,0 @@
-class DropChatrooms < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :chatrooms
-  end
-end
